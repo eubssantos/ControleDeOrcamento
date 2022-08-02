a0c954d61190bcc50460bd446cf03c2b67d9e556
Bruna <eubssantos@outlook.com>
2022-08-02 14:04:09 -0300

    Initial commit

---
+++ src/main/java/com/alura/controledeorcamento/ControleDeOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package com.alura.controledeorcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeOrcamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleDeOrcamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alura/controledeorcamento/ControleDeOrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.controledeorcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeOrcamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
