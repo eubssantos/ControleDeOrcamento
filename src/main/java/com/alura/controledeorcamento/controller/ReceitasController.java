1d7f746bd37b0bd10d55179424c6b1c92e89c789
Bruna <eubssantos@outlook.com>
2022-08-03 00:15:23 -0300

    add listagemDeReceitas;

--- src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
+++ src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
@@ -1,11 +1,14 @@
 package com.alura.controledeorcamento.controller;
 
+import com.alura.controledeorcamento.repository.entity.Receitas;
 import com.alura.controledeorcamento.service.ReceitasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/receitas")
@@ -15,7 +18,7 @@ public class ReceitasController {
     ReceitasService receitasService;
 
     @GetMapping("/listagem")
-    public ResponseEntity<?> listagemDeReceitas() {
-        return ResponseEntity.ok(receitasService.listagemDeReceitas());
+    public ResponseEntity<List<Receitas>> listagemDeReceitas() {
+        return ResponseEntity.ok().body(receitasService.getAll());
     }
 }
