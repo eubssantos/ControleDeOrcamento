9d9cdd2e77c4b1bb2b088fcf580538b593a2fab2
Bruna <eubssantos@outlook.com>
2022-08-03 00:12:24 -0300

    add listagemDeReceitas;

--- src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
+++ src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
@@ -1,4 +1,21 @@
 package com.alura.controledeorcamento.controller;
 
+import com.alura.controledeorcamento.service.ReceitasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/receitas")
 public class ReceitasController {
+
+    @Autowired
+    ReceitasService receitasService;
+
+    @GetMapping("/listagem")
+    public ResponseEntity<?> listagemDeReceitas() {
+        return ResponseEntity.ok(receitasService.listagemDeReceitas());
+    }
 }
