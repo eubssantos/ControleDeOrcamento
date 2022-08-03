680bfdbf84467df8430d7a45b75584a7522379a5
Bruna <eubssantos@outlook.com>
2022-08-03 18:48:35 -0300

    add cadastroDeReceitas

--- src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
+++ src/main/java/com/alura/controledeorcamento/controller/ReceitasController.java
@@ -1,14 +1,14 @@
 package com.alura.controledeorcamento.controller;
 
-import com.alura.controledeorcamento.repository.entity.Receitas;
+import com.alura.controledeorcamento.entity.Receitas;
 import com.alura.controledeorcamento.service.ReceitasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/receitas")
@@ -20,5 +20,15 @@ public class ReceitasController {
     @GetMapping("/listagem")
     public ResponseEntity<List<Receitas>> listagemDeReceitas() {
         return ResponseEntity.ok().body(receitasService.getAll());
+    }
+
+    @GetMapping(value = "/listagem/{id}")
+    public ResponseEntity<Optional<Receitas>> detalhamentoDeReceita(@PathVariable Long id) {
+        return ResponseEntity.ok().body(receitasService.getById(id));
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Receitas> cadastroDeReceitas(@RequestBody Receitas receitas) {
+        return ResponseEntity.ok().body(receitasService.create(receitas));
     }
 }
