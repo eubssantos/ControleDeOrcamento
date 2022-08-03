88a8301d3a0671617cf156f56dcf5049ac2459f0
Bruna <eubssantos@outlook.com>
2022-08-03 18:48:21 -0300

    add cadastroDeDespesas

--- src/main/java/com/alura/controledeorcamento/controller/DespesasController.java
+++ src/main/java/com/alura/controledeorcamento/controller/DespesasController.java
@@ -1,15 +1,14 @@
 package com.alura.controledeorcamento.controller;
 
-import com.alura.controledeorcamento.repository.entity.Despesas;
+import com.alura.controledeorcamento.entity.Despesas;
 import com.alura.controledeorcamento.service.DespesasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/despesas")
@@ -21,6 +20,16 @@ public class DespesasController {
     @GetMapping(value = "/listagem")
     public ResponseEntity<List<Despesas>> listagemDeDespesas() {
         return ResponseEntity.ok().body(despesasService.getAll());
+    }
+
+    @GetMapping(value = "/listagem/{id}")
+    public ResponseEntity<Optional<Despesas>> detalhamentoDeDespesa(@PathVariable Long id) {
+        return ResponseEntity.ok().body(despesasService.getById(id));
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Despesas> cadastroDeDespesas(@RequestBody Despesas despesas) {
+        return ResponseEntity.ok().body(despesasService.saveDespesas(despesas));
     }
 
 }
