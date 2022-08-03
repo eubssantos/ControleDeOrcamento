6f2658166810ef9e464159c9c5dc455e8e6388b5
Bruna <eubssantos@outlook.com>
2022-08-03 00:12:09 -0300

    add listagemDeDespesas;

--- src/main/java/com/alura/controledeorcamento/controller/DespesasController.java
+++ src/main/java/com/alura/controledeorcamento/controller/DespesasController.java
@@ -1,4 +1,26 @@
 package com.alura.controledeorcamento.controller;
 
+import com.alura.controledeorcamento.repository.entity.Despesas;
+import com.alura.controledeorcamento.service.DespesasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/despesas")
 public class DespesasController {
+
+    @Autowired
+    DespesasService despesasService;
+
+    @GetMapping(value = "/listagem")
+    public ResponseEntity<List<Despesas>> listagemDeDespesas() {
+        return ResponseEntity.ok().body(despesasService.getAll());
+    }
+
 }
