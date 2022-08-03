8a417eef7cca82f20d4dde2f21d702710f5200d9
Bruna <eubssantos@outlook.com>
2022-08-03 00:13:00 -0300

    add getAll;

--- src/main/java/com/alura/controledeorcamento/service/DespesasService.java
+++ src/main/java/com/alura/controledeorcamento/service/DespesasService.java
@@ -1,4 +1,21 @@
 package com.alura.controledeorcamento.service;
 
+import com.alura.controledeorcamento.repository.entity.Despesas;
+import com.alura.controledeorcamento.repository.DespesasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DespesasService {
+
+    @Autowired
+    DespesasRepository despesasRepository;
+
+    public List<Despesas> getAll() {
+        List<Despesas> list = despesasRepository.findAll();
+       return list;
+    }
+
 }
--- src/main/java/com/alura/controledeorcamento/service/ReceitasService.java
+++ src/main/java/com/alura/controledeorcamento/service/ReceitasService.java
@@ -1,4 +1,19 @@
 package com.alura.controledeorcamento.service;
 
+import com.alura.controledeorcamento.repository.entity.Receitas;
+import com.alura.controledeorcamento.repository.ReceitasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReceitasService {
+
+    @Autowired
+    ReceitasRepository receitasRepository;
+
+    public List<Receitas> listagemDeReceitas() {
+        return receitasRepository.findAll();
+    }
 }
